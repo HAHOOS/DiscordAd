2b19fe58e2beb020cccd953d9565ea76078ed32a
HAHOOS <gracjan.dwornicki@gmail.com>
2021-06-22 12:59:11 +0200

    AssemblyInfo update

--- Discord Ad/Properties/AssemblyInfo.cs
+++ Discord Ad/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Discord Ad")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DiscordAd")]
+[assembly: AssemblyDescription("DiscordAd is program to advertisement your e.x youtube channel")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Discord Ad")]
